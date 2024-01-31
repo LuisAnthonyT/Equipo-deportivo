62d5c0d388b97b13439fbbf9df5426c4e1c260a4
luis anthony <luitoabol@alu.edu.gva.es>
2024-01-31 17:28:52 +0100

    MCR DE TODAS LAS TABLAS HECHO

---
+++ database/migrations/2024_01_31_154206_create_players_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 15);
+            $table->text('twitter')->nullable();
+            $table->text('instagram')->nullable();
+            $table->text('twitch')->nullable();
+            $table->boolean('visible')->default(false);
+            //Campo adicionales
+            $table->string('position')->nullable(); //Delantero, defensa, etc
+            $table->integer('jersey_number')->nullable(); //10, 2, etc
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
